97ddd9810893e648470d2e0ae90b9674501f03bb
ldpc520 <397639558@qq.com>
2026-05-28 06:32:17 +0800

    Update sidebarConfig.ts

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -5,13 +5,13 @@ import type { SidebarLayoutConfig } from "../types/config";
  */
 export const sidebarLayoutConfig: SidebarLayoutConfig = {
 	// 是否启用侧边栏功能
-	enable: false,
+	enable: true,
 
 	// 侧边栏位置：
 	// left: 仅显示左侧边栏
 	// right: 仅显示右侧边栏
 	// both: 双侧边栏，1280px以上同时显示左右，769-1279px根据tabletSidebar配置显示其中一侧
-	position: "both",
+	position: "left",
 
 	// 平板端(769-1279px)显示哪侧侧边栏，仅position为both时生效
 	// left: 平板端显示左侧边栏
